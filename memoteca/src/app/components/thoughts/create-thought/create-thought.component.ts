13f250883146c3b1739af9e1717263c067f92f06
Yuri Cruz <yuricruzf@gmail.com>
2023-10-24 20:47:12 -0300

    Creating object with values

--- memoteca/src/app/components/thoughts/create-thought/create-thought.component.ts
+++ memoteca/src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-thought.component.css']
 })
 export class CreateThoughtComponent {
-
+  pensamento = {
+    id: '1',
+    conteudo: "Aprendendo Angular",
+    autoria: "Dev",
+    modelo: ""
+  }
 }
