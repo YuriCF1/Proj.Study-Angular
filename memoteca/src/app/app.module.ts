1e8bccb1f1c2f7c4e39969b5bea78cf9f60e9b5b
Yuri Cruz <yuricruzf@gmail.com>
2023-10-26 22:56:08 -0300

    Importing FormModule

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { CreateThoughtComponent } from './components/thoughts/create-thought/cre
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Preciso importar o FormsModule abaixo para que o ngModule funciona no input
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
