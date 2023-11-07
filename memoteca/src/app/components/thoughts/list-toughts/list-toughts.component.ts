7387b44826ca9fc515be7e02d969ee852fd479fe
Yuri Cruz <yuricruzf@gmail.com>
2023-11-07 19:11:02 -0300

    Simulating data from backend

--- memoteca/src/app/components/thoughts/list-toughts/list-toughts.component.ts
+++ memoteca/src/app/components/thoughts/list-toughts/list-toughts.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-toughts.component.css']
 })
 export class ListToughtsComponent {
+  listThought = [{
+    conteudo: "Passo informações para o componente filho",
+    autoria: "Componente pai",
+    modelo: "modelo3"
+  }, {
+    conteudo: "Minha propriedade é decorado com @Input",
+    autoria: "Componente filho",
+    modelo: "modelo2"
+  }]
 
 }
