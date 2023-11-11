9ae33da83320ce727791fa1d90e1ed66bf729fb3
Yuri Cruz <yuricruzf@gmail.com>
2023-11-10 21:53:11 -0300

    Testing card to add class dinamically

--- memoteca/src/app/components/thoughts/list-toughts/list-toughts.component.ts
+++ memoteca/src/app/components/thoughts/list-toughts/list-toughts.component.ts
@@ -7,15 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ListToughtsComponent {
   listThought = [
-  //   {
-  //   conteudo: "Passo informações para o componente filho",
-  //   autoria: "Componente pai",
-  //   modelo: "modelo3"
-  // }, {
-  //   conteudo: "Minha propriedade é decorado com @Input",
-  //   autoria: "Componente filho",
-  //   modelo: "modelo2"
-  // }
-]
+    {
+      conteudo: "Passo informações para o componente filho",
+      autoria: "Componente pai",
+      modelo: "modelo3"
+    }, {
+      conteudo: "Minha propriedade é decorado com @Input",
+      autoria: "Componente filho",
+      modelo: "modelo2"
+    }
+    , {
+      conteudo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate vehicula dolor, at tempor elit malesuada ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque turpis tellus, imperdiet ut neque nec, feugiat vehicula justo. Aliquam ut velit felis. Sed iaculis ac lorem eu dapibus. Donec ac semper quam. Curabitur interdum urna eget urna tincidunt, a consectetur augue pulvinar. Aenean rutrum felis eget leo bibendum venenatis. Integer et pulvinar quam, bibendum sollicitudin augue. Suspendisse potenti. Mauris lectus justo, placerat quis pulvinar sit amet, scelerisque eget massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin consectetur hendrerit arcu, eget luctus urna semper ac. Vestibulum mauris erat, mattis eget orci commodo, mattis varius metus. Maecenas fermentum rutrum est, id fermentum libero tempus id. Curabitur ut auctor quam.",
+      autoria: "Componente filho",
+      modelo: "modelo2"
+    }
+  ]
 
 }
