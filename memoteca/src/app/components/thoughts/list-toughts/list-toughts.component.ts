50c501b9758c85e63aae11a409c16e6f44b3ce01
Yuri Cruz <yuricruzf@gmail.com>
2023-11-11 17:33:08 -0300

    Creating ngOnInit

--- memoteca/src/app/components/thoughts/list-toughts/list-toughts.component.ts
+++ memoteca/src/app/components/thoughts/list-toughts/list-toughts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThoughtInterface } from '../ITthought';
+import { ThoughtServiceService } from '../thought-service.service';
 
 @Component({
   selector: 'app-list-toughts',
@@ -6,21 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-toughts.component.css']
 })
 export class ListToughtsComponent {
-  listThought = [
-    {
-      conteudo: "Passo informações para o componente filho",
-      autoria: "Componente pai",
-      modelo: "modelo3"
-    }, {
-      conteudo: "Minha propriedade é decorado com @Input",
-      autoria: "Componente filho",
-      modelo: "modelo2"
-    }
-    , {
-      conteudo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate vehicula dolor, at tempor elit malesuada ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque turpis tellus, imperdiet ut neque nec, feugiat vehicula justo. Aliquam ut velit felis. Sed iaculis ac lorem eu dapibus. Donec ac semper quam. Curabitur interdum urna eget urna tincidunt, a consectetur augue pulvinar. Aenean rutrum felis eget leo bibendum venenatis. Integer et pulvinar quam, bibendum sollicitudin augue. Suspendisse potenti. Mauris lectus justo, placerat quis pulvinar sit amet, scelerisque eget massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin consectetur hendrerit arcu, eget luctus urna semper ac. Vestibulum mauris erat, mattis eget orci commodo, mattis varius metus. Maecenas fermentum rutrum est, id fermentum libero tempus id. Curabitur ut auctor quam.",
-      autoria: "Componente filho",
-      modelo: "modelo2"
-    }
+  listThought: ThoughtInterface[] = [
+    // {
+    //   id: 1,
+    //   conteudo: "Passo informações para o componente filho",
+    //   autoria: "Componente pai",
+    //   modelo: "modelo3"
+    // }, {
+    //   id: 2,
+    //   conteudo: "Minha propriedade é decorado com @Input",
+    //   autoria: "Componente filho",
+    //   modelo: "modelo2"
+    // }
+    // , {
+    //   id: 3,
+    //   conteudo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate vehicula dolor, at tempor elit malesuada ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque turpis tellus, imperdiet ut neque nec, feugiat vehicula justo. Aliquam ut velit felis. Sed iaculis ac lorem eu dapibus. Donec ac semper quam. Curabitur interdum urna eget urna tincidunt, a consectetur augue pulvinar. Aenean rutrum felis eget leo bibendum venenatis. Integer et pulvinar quam, bibendum sollicitudin augue. Suspendisse potenti. Mauris lectus justo, placerat quis pulvinar sit amet, scelerisque eget massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin consectetur hendrerit arcu, eget luctus urna semper ac. Vestibulum mauris erat, mattis eget orci commodo, mattis varius metus. Maecenas fermentum rutrum est, id fermentum libero tempus id. Curabitur ut auctor quam.",
+    //   autoria: "Componente filho",
+    //   modelo: "modelo2"
+    // }
   ]
 
+  constructor(private service: ThoughtServiceService) {
+
+  }
+
+  //ngOnInit executa o algoritmo assim que o component é iniciado
+  ngOnInit(): void {
+    this.service.listIt()
+  }
 }
