d6173fe1fedc8864d231a2daf7e267ec45b021e8
Yuri Cruz <yuricruzf@gmail.com>
2023-11-13 21:47:13 -0300

    Setting dinamic route to get the id

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 import { ListToughtsComponent } from './components/thoughts/list-toughts/list-toughts.component';
+import { DeleteToughtComponent } from './components/thoughts/delete-thought/delete-thought.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
     path: 'create-thought',
     component: CreateThoughtComponent
   },
+  {
+    path: 'pensamentos/delete-thought/:id',
+    component: DeleteToughtComponent
+  }
 ];
 
 @NgModule({
