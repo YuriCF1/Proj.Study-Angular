68acaa26bfdb031dc4961b46a7f6e3918d57272e
Yuri Cruz <yuricruzf@gmail.com>
2023-12-08 15:19:52 -0300

    Adding service to call function and favorite property

--- memoteca/src/app/components/thoughts/thought/thought.component.ts
+++ memoteca/src/app/components/thoughts/thought/thought.component.ts
@@ -1,5 +1,7 @@
+import { Observable } from 'rxjs';
+import { ThoughtInterface } from './../ITthought';
 import { Component, Input } from '@angular/core';
-import { ThoughtInterface } from '../ITthought';
+import { ThoughtService } from '../thought.service';
 
 @Component({
   selector: 'app-thought',
@@ -7,20 +9,35 @@ import { ThoughtInterface } from '../ITthought';
   styleUrls: ['./thought.component.css']
 })
 export class ThoughtComponent {
+
+  constructor(private service: ThoughtService) {
+  }
+
   // O Input diz que a proprieade pensamento, receberá informações de componente pai, e não daqui
   @Input() pensamento: ThoughtInterface = {
     id: 0,
     conteudo: "I love Angular",
     autoria: "Yuri Cruz",
-    modelo: "modelo1"
+    modelo: "modelo1",
+    favorite: false
   }
 
   larguraPensamento(): string {
     if (this.pensamento.conteudo.length > 256) {
       return "pensamento-g"
     }
-
     return "pensamento-p"
   }
 
+  changeFavoriteIcon(): string {
+    if (this.pensamento.favorite == false) {
+      return 'inativo'
+    } else {
+      return 'ativo'
+    }
+  }
+
+  updateFavoriteList() {
+    this.service.changeFavoriteStatus(this.pensamento).subscribe();
+  }
 }
